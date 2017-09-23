2644cf77a181e7ddae6db9a1cd5dd260be551e55
mehdi.byt@gmail.com <mehdi.byt@gmail.com>
2017-09-24 00:52:27 +0330

    init

---
+++ barbari/barbari/Controllers/HomeController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Mvc;
+
+namespace barbari.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ barbari/barbari/Controllers/PagesController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Mvc;
+
+namespace barbari.Controllers
+{
+    public class PagesController : Controller
+    {
+        public ActionResult single()
+        {
+            return View();
+        }
+    }
+}
